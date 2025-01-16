a1ebc5f5b5bdbb07e4b9615b421ce1ff36b6aab2
yogita <yogikohli7100@gmail.com>
2025-01-16 11:55:35 +0530

    16-01-2025

---
+++ 2024.py
@@ -0,0 +1,374 @@
+from turtle import *
+win = Screen()
+tess = Turtle()
+win.bgcolor("black")
+tess.pensize(5) 
+colors="white"
+tess.penup()
+tess.goto(-100,20)
+tess.pendown()
+tess.color(colors)
+tess.fillcolor(colors)
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+       
+
+tess.end_fill()
+
+
+
+tess.penup()
+tess.goto(0,50)
+tess.pendown()
+tess.color("red")
+tess.fillcolor("red")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+       
+
+tess.end_fill()
+
+
+
+
+tess.penup()
+tess.goto(-100,-100)
+tess.pendown()
+tess.color("blue")
+tess.fillcolor("blue")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+       
+
+tess.end_fill()
+
+
+
+tess.penup()
+tess.goto(-300,-200)
+tess.pendown()
+tess.color(colors)
+tess.fillcolor(colors)
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+       
+
+tess.end_fill()
+
+
+
+tess.penup()
+tess.goto(-400,100)
+tess.pendown()
+tess.color("gold")
+tess.fillcolor("gold")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+       
+
+tess.end_fill()
+
+
+tess.penup()
+tess.goto(-400,-200)
+tess.pendown()
+tess.color("coral")
+tess.fillcolor("coral")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+       
+
+tess.end_fill()
+
+
+tess.penup()
+tess.goto(-310, 330)
+tess.pendown()
+tess.color("red")
+tess.right(90)
+tess.forward(229)
+tess.penup()
+tess.color('red')
+tess.goto(-330, 40)
+tess.write("H ",  font=("times new roman", 70, "italic"))
+
+tess.penup()
+tess.goto(-250, 330)
+tess.pendown()
+tess.color("blue")
+tess.forward(240)
+tess.penup()
+tess.color('blue')
+tess.goto(-280, 20)
+tess.write("A ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(-200, 330)
+tess.pendown()
+tess.color("pink")
+tess.forward(205)
+tess.penup()
+tess.color('pink')
+tess.goto(-230, 40)
+tess.write("P ",  font=("times new roman", 70,"italic"))
+
+tess.penup()
+tess.goto(-150, 330)
+tess.pendown()
+tess.color("gold")
+tess.forward(205)
+tess.penup()
+tess.color('gold')
+tess.goto(-178, 40)
+tess.write("P ",  font=("times new roman", 70,"italic"))
+
+tess.penup()
+tess.goto(-68, 330)
+tess.pendown()
+tess.color("green")
+tess.forward(205)
+tess.penup()
+tess.color('green')
+tess.goto(-120, 40)
+tess.write("Y ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(10, 330)
+tess.pendown()
+tess.color("grey")
+tess.forward(205)
+tess.penup()
+tess.color('grey')
+tess.goto(-0, 40)
+tess.write("N ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(80, 330)
+tess.pendown()
+tess.color("yellow")
+tess.forward(225)
+tess.penup()
+tess.color('yellow')
+tess.goto(60, 20)
+tess.write("E ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(165, 330)
+tess.pendown()
+tess.color("red")
+tess.forward(205)
+tess.penup()
+tess.color('red')
+tess.goto(120, 40)
+tess.write("W ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(270, 330)
+tess.pendown()
+tess.color("deep pink")
+tess.forward(205)
+tess.penup()
+tess.color('deep pink')
+tess.goto(260, 40)
+tess.write("Y ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(340, 330)
+tess.pendown()
+tess.color("grey")
+tess.forward(205)
+tess.penup()
+tess.color('grey')
+tess.goto(320, 40)
+tess.write("E ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(400, 330)
+tess.pendown()
+tess.color("brown")
+tess.forward(240)
+tess.penup()
+tess.color('brown')
+tess.goto(370, 20)
+tess.write("A ",  font=("times new roman", 70,"italic"))
+
+
+tess.penup()
+tess.goto(440, 330)
+tess.pendown()
+tess.color("pink")
+tess.forward(205)
+tess.penup()
+tess.color('pink')
+tess.goto(420, 40)
+tess.write("R ",  font=("times new roman", 70,"italic"))
+
+
+
+
+
+
+tess.penup()
+tess.goto(250,200)
+tess.pendown()
+tess.color(colors)
+tess.fillcolor(colors)
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+tess.end_fill()
+
+
+
+tess.penup()
+tess.goto(400,-250)
+tess.pendown()
+tess.color("deep pink")
+tess.fillcolor("deep pink")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+tess.end_fill()
+
+
+tess.penup()
+tess.goto(250,-200)
+tess.pendown()
+tess.color("grey")
+tess.fillcolor("grey")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+tess.end_fill()
+
+
+
+
+win.bgpic("background.gif")
+
+
+tess.penup()
+tess.goto(0,0)
+tess.pendown()
+tess.color("deep pink")
+tess.fillcolor("deep pink")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+tess.end_fill()
+
+
+tess.penup()
+tess.goto(70,-100)
+tess.pendown()
+tess.color("green")
+tess.fillcolor("green")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+tess.end_fill()
+
+
+tess.penup()
+tess.goto(200,-200)
+tess.pendown()
+tess.color("coral")
+tess.fillcolor("coral")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+tess.end_fill()
+
+
+tess.penup()
+tess.goto(0,-200)
+tess.pendown()
+tess.color("gold")
+tess.fillcolor("gold")
+tess.begin_fill()
+for i in range(5):
+        
+        # moving turtle 100 units forward
+        tess.forward(10)
+        
+        # rotating turtle 144 degree right
+        tess.right(144)
+tess.end_fill()
+
+
+win.addshape("2024.gif")
+tess.shape("2024.gif")
+win.mainloop()
